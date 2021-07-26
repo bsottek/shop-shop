9f56c6e9d2eea4218d4ef4c5bc151440570ec2d4
bsottek <bsottek@gmail.com>
2021-07-26 03:11:35 -0500

    schema and seed refactor

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
 
+const handleError = error => console.log(error);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
+}).
+  catch(error => handleError(error));
+
+const logError = err => console.log(err);
+
+mongoose.connection.on('error', err => {
+  logError(err);
 });
 
 module.exports = mongoose.connection;
